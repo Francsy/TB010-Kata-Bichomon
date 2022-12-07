e95c5a39dc8b5b3627b5f635d147f1c91997d6cd
Francsy <francsyludens@gmail.com>
2022-12-08 00:20:51 +0100

    Añadida una nueva opción comprobada al último ejercicio: Seleccionando directamente a los elementos parent que tienen un hijo específico

--- scripts.js
+++ scripts.js
@@ -9,33 +9,22 @@ document.getElementById("gen-1").innerHTML = "Generasion 1 Pokimon";
 genList = document.querySelectorAll("main>div.infocard-list")
 genList[0].style.backgroundColor = "blue";
 
-
 // Mismo método sin declarar variable:
 //document.querySelectorAll("main>div.infocard-list")[0]].style.backgroundColor = "red";
 // Otra forma:
 //document.querySelector("main>div.infocard-list").style.backgroundColor = "red";
 
-
-
 //IMPRIME POR CONSOLA LA URL DE LA PAGINA:
 
 console.log(document.URL);
-
-
 
 //IMPRIME POR CONSOLA EL DOMINIO DE LA PAGINA:
 
 console.log(document.domain);
 
-
-
 //IMPRIME TODOS LOS NODOS DE IMAGEN
 
 console.log(document.images)
-
-
-
-
 
 //SUSTITUYE EL ATRIBUTO "SRC" DE TODAS LAS IMAGENES POR ESTE "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
 
@@ -44,19 +33,20 @@ for (i = 0 ; i < imgAll.length ; i++) {
     imgAll[i].setAttribute("src","https://media.giphy.com/media/2v170e71aanfi/giphy.gif")
 }
 
-
-
-
-
-
-
 //Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itipe flying
 
+// Seleccionando directamente a los elemento parent que tiene dicho elemento hijo:
 
-let infoCards = document.querySelectorAll(".infocard-lg-data.text-muted")
+let infoCards = document.querySelectorAll("span:has(.itype.flying)")
 for (i = 0 ; i < infoCards.length ; i++){
-    if (infoCards[i].innerHTML.includes("flying")){
-        infoCards[i].style.backgroundColor = "red";
-    }
+infoCards[i].style.backgroundColor = "red";
 }
-    
+
+// Haciendo una selección más genérica y poniendo la condición de que lo incluyan:
+
+// let infoCards = document.querySelectorAll(".infocard-lg-data.text-muted")
+// for (i = 0 ; i < infoCards.length ; i++){
+//     if (infoCards[i].innerHTML.includes("flying")){
+//         infoCards[i].style.backgroundColor = "red";
+//     }
+// }
